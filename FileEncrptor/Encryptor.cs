3fdccda546a27ccae46aeb6b6d52fc8298d387d6
Astyr <astyr.github@gmail.com>
2020-08-12 16:10:30 +0200

    Initial commit

---
+++ FileEncrptor/Encryptor.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileEncrptor
+{
+    public class Encryptor
+    {
+        //  Call this function to remove the key from memory after use for security
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr destination, int length);
+
+        /// <summary>
+        /// Creates a random salt that will be used to encrypt your file. This method is required on FileEncrypt.
+        /// </summary>
+        /// <returns></returns>
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static byte[] GenerateRandomSalt()
+        {
+            var data = new byte[32];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    // Fill the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+    }
+
+    /// <summary>
+    /// Encrypts a file from its path and a plain password.
+    /// </summary>
+    /// <param name="inputFile"></param>
+    /// <param name="password"></param>
+    public static void FileEncrypt(string inputFile, string password)
+    {
+        //generate random salt
+        var salt = GenerateRandomSalt();
+
+        //create output file name
+        var fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+        //convert password string to byte array
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        //Set Rijndael symmetric encryption algorithm
+        // ReSharper disable once InconsistentNaming
+        var AES = new RijndaelManaged {KeySize = 256, BlockSize = 128, Padding = PaddingMode.PKCS7};
+
+        var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+        AES.Key = key.GetBytes(AES.KeySize / 8);
+        AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+        //Cipher mode
+        AES.Mode = CipherMode.CFB;
+
+        // write salt to the beginning of the output file, so in this case can be random every time
+        fsCrypt.Write(salt, 0, salt.Length);
+
+        var cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+        var fsIn = new FileStream(inputFile, FileMode.Open);
+
+        //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+        var buffer = new byte[1048576];
+        // ReSharper disable once TooWideLocalVariableScope
+        int read;
+
+        try
+        {
+            while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                //Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                cs.Write(buffer, 0, read);
+            }
+
+            // Close up
+            fsIn.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(@"Error: " + ex.Message);
+        }
+        finally
+        {
+            cs.Close();
+            fsCrypt.Close();
+        }
+    }
+
+    /// <summary>
+    /// Decrypts an encrypted file with the FileEncrypt method through its path and the plain password.
+    /// </summary>
+    /// <param name="inputFile"></param>
+    /// <param name="outputFile"></param>
+    /// <param name="password"></param>
+    public static void FileDecrypt(string inputFile, string outputFile, string password)
+    {
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var salt = new byte[32];
+
+        var fsCrypt = new FileStream(inputFile, FileMode.Open);
+        fsCrypt.Read(salt, 0, salt.Length);
+
+        // ReSharper disable once InconsistentNaming
+        var AES = new RijndaelManaged {KeySize = 256, BlockSize = 128};
+        var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+        AES.Key = key.GetBytes(AES.KeySize / 8);
+        AES.IV = key.GetBytes(AES.BlockSize / 8);
+        AES.Padding = PaddingMode.PKCS7;
+        AES.Mode = CipherMode.CFB;
+
+        var cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+        var fsOut = new FileStream(outputFile, FileMode.Create);
+
+        // ReSharper disable once TooWideLocalVariableScope
+        int read;
+        var buffer = new byte[1048576];
+
+        try
+        {
+            while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                //Application.DoEvents();
+                fsOut.Write(buffer, 0, read);
+            }
+        }
+        // ReSharper disable once InconsistentNaming
+        catch (CryptographicException ex_CryptographicException)
+        {
+            Console.WriteLine(@"CryptographicException error: " + ex_CryptographicException.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(@"Error: " + ex.Message);
+        }
+
+        try
+        {
+            cs.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(@"Error by closing CryptoStream: " + ex.Message);
+        }
+        finally
+        {
+            fsOut.Close();
+            fsCrypt.Close();
+        }
+    }
+    
+    }
+}
---
+++ FileEncrptor/MainWindow.xaml.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using Microsoft.WinAny.IO;
+using static FileEncrptor.Encryptor;
+
+namespace FileEncrptor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+        /// <summary>
+        /// File management buttons logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileAddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create a file selector
+            var openFileDlg = new OpenFileDialog
+            {
+                Multiselect = true, CheckFileExists = true, CheckPathExists = true, Filter = "All files (*.*)|*.*"
+            };
+
+            // Open the file selector dialog
+            var result = openFileDlg.ShowDialog();
+
+            // Check for results and add items to the list
+            if (result != true) return;
+            foreach (var s in openFileDlg.FileNames)
+                FileList.Items.Add(s);
+        }
+        
+        private void RemoveSelected_OnClick(object sender, RoutedEventArgs e)
+        {
+            // CHeck if there is selected items
+            if (FileList.SelectedItems.Count == 0) return;
+            
+            // Delete the selected items
+            for (var i = FileList.Items.Count - 1; i >= 0; i--)
+            {
+                FileList.Items.RemoveAt(i);
+            }
+        }
+        
+        
+        
+        /// <summary>
+        /// Password text box logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Dynamically enable or disable buttons
+            if (FileList.Items.Count == 0 && (PasswordBox.Text == "Password" ||
+                                              string.IsNullOrEmpty(PasswordBox.Text) ||
+                                              string.IsNullOrWhiteSpace(PasswordBox.Text)))
+            {
+                EncryptButton.IsEnabled = false;
+                DecryptButton.IsEnabled = false;                
+            }
+            else
+            {
+                EncryptButton.IsEnabled = true;
+                DecryptButton.IsEnabled = true;
+            }
+        }
+        
+        private void PasswordBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            // Remove placeholder text
+            if (PasswordBox.Text == "Password")
+                PasswordBox.Text = "";
+        }
+
+        private void PasswordBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            // Add placeholder text
+            if (PasswordBox.Text == "")
+                PasswordBox.Text = "Password";
+        }
+             
+        
+        
+        /// <summary>
+        /// Remove files after operation checkbox logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RmvFileAftEncrypt(object sender, RoutedEventArgs e)
+        {
+            // Prevent the dialog from being showed if check is being unchecked
+            if (RmvFileAftEncryptCheckbox.IsChecked == false) return;
+            
+            RmvFileAftEncryptCheckbox.IsChecked = false;
+            
+            // Warning prompt
+            var messageBoxResult = MessageBox.Show(
+                "Warning, files will be lost forever !\nPlease note that this apply to decryption, where encrypted files will be deleted.\n\nProceed ?",
+                "Warning !", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+            // If user wants to proceed, checkbox is checked
+            if (messageBoxResult == MessageBoxResult.Yes)
+                RmvFileAftEncryptCheckbox.IsChecked = true;
+        }
+        
+        
+        
+        /// <summary>
+        /// Custom method to append bytes to a file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="bytes"></param>
+        private static void AppendAllBytes(string path, byte[] bytes)
+        {
+            using (var stream = new FileStream(path, FileMode.Append))
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Markup byte array
+        /// </summary>
+        private readonly byte[] _markupByteArray = {
+            0x43, 0x72, 0x79, 0x70, 0x74, 0x65, 0x64, 0x20, 0x62, 0x79, 0x20, 0x46, 0x69, 0x6c, 0x65, 0x45, 0x6e, 0x63,
+            0x72, 0x79, 0x70, 0x74, 0x6f, 0x72, 0x20, 0x6d, 0x61, 0x64, 0x65, 0x20, 0x62, 0x79, 0x20, 0x41, 0x73, 0x74,
+            0x79, 0x72, 0x23, 0x33, 0x35, 0x33, 0x35
+        };
+
+        private bool FindMarkupByte(string filePath)
+        {
+            var s = ""; // s variable to compare markup bytes and result
+            
+            // Method to read the last 43 bytes
+            using (var reader = new StreamReader(filePath))
+            {
+                if (reader.BaseStream.Length > 43)
+                {
+                    reader.BaseStream.Seek(-43, SeekOrigin.End);
+                }
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    s = line;
+                }
+            }
+            
+            // Check if found bytes correspond to the markup bytes
+            if (s == "Crypted by FileEncryptor made by Astyr#3535")
+                return true;
+            
+            return false; // Return false if not
+        }
+
+        /// <summary>
+        /// Encrypt button logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EncryptButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //This bool is a fail-safe variable to prevent files from being deleted if an error occurs
+            var removeFiles = true;
+
+            //Confirmation dialog
+            var messageBoxResult = MessageBox.Show(
+                "You are about to encrypt files !\nPlease note that passwords cannot be recovered in anyway.\n\nProceed ?",
+                "Warning !",
+                MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+            // Check if user wants to proceed and if password isn't null 
+            if (messageBoxResult == MessageBoxResult.Yes && PasswordBox.Text != "")
+            {
+                // List through all the items in the list
+                foreach (var v in FileList.Items)
+                {
+                    var s = v.ToString();
+
+                    // Check for the extension to not be "aes"
+                    if (s.Substring(s.Length - 3) != "aes")
+                    {
+                        try
+                        {
+                            AppendAllBytes(v.ToString(), _markupByteArray);
+                            FileEncrypt(v.ToString(), PasswordBox.Text);
+                        }
+                        catch (Exception exception)
+                        {
+                            removeFiles = false;
+                            MessageBox.Show(exception.ToString());
+                        }
+
+                    }
+
+                    // If selected, delete the file after encryption using a secure algorithm
+                    if (RmvFileAftEncryptCheckbox.IsChecked == true && removeFiles)
+                    {
+                        var fi1 = new FileInfo(v.ToString());
+                        fi1.Delete(OverwriteAlgorithm.Random);
+                    }
+                }
+                // If the operation has been successful, then show final message
+                if (removeFiles)
+                {
+                    FileList.Items.Clear();
+                    MessageBox.Show("Encryption done !", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+                    PasswordBox.Text = "";
+                }
+            }
+        }
+        
+        
+        
+        /// <summary>
+        /// Decrypt button logic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DecryptButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //This bool is a fail-safe variable to prevent files from being deleted if an error occurs
+            var removeFiles = true;
+            
+            // Confirmation dialog
+            var messageBoxResult = MessageBox.Show(
+                "You are about to decrypt files !\nPLease note that invalid password are not always detected and can cause corrupted files.\nProceed ?",
+                "Warning !",
+                MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+            try
+            {
+                // Check if the user wants to proceed
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+                    // List through all of the items
+                    foreach (var v in FileList.Items)
+                    {
+                        var s = v.ToString();
+
+                        // Check for extension to be "aes"
+                        if (s.Substring(s.Length - 3) == "aes")
+                        {
+                            // New file path creation
+                            var newFile = v.ToString().Replace(".aes", "");
+
+                            // Decryption
+                            FileDecrypt(v.ToString(), newFile, PasswordBox.Text);
+
+                            // Check if file has been successfully decrypted by reading the markup byte
+                            if (!FindMarkupByte(newFile))
+                            {
+                                // Show an error message and prevent the files from being deleted if an error occurs
+                                MessageBox.Show("Oops, an error as occured, maybe a wrong password ?", "Error !",
+                                    MessageBoxButton.OK, MessageBoxImage.Error);
+                                var fi2 = new FileInfo(newFile);
+                                fi2.Delete(OverwriteAlgorithm.Quick);
+                                removeFiles = false;
+                                break;
+                            }
+
+                            // Remove files if asked
+                            if (removeFiles && RmvFileAftEncryptCheckbox.IsChecked == true)
+                            {
+                                var fi1 = new FileInfo(v.ToString());
+                                fi1.Delete(OverwriteAlgorithm.Random);
+                            }
+                        }
+                    }
+                    // Successful operation message
+                    if (removeFiles)
+                    {
+                        MessageBox.Show("Decryption done !", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+                        FileList.Items.Clear();
+                        PasswordBox.Text = "";
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+                throw;
+            }
+        }
+    }
+}
